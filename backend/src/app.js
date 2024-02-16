b6218847987ddd6896f1662805f776165c587004
PETACCIA <petaccia.seb@gmail.com>
2024-02-16 12:11:43 +0100

    Mise a jour de la branch main

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 // use some application-level middlewares
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
+    origin: process.env.FRONTEND_URL ?? "pokebiz.netlify.app",
     optionsSuccessStatus: 200,
   })
 );
